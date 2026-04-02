0ede1b75f03510b5053bcd9229dd57e7db0143fa
Chayon Bain <chayonbain77@gmail.com>
2026-04-02 22:17:03 +0600

    Create Watermelon.cpp

---
+++ 800_rating_problems/Watermelon.cpp
@@ -0,0 +1,14 @@
+//problem link:https://codeforces.com/contest/4/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w
+    if(w%2==0 && w>=4)
+    cout<<"YES"<<endl;
+    else
+   cout<<"NO"<<endl;
+    
+    return 0;
+}
